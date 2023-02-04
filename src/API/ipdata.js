eb69fb7f6926b7c95fe9be7e3e33ec8954c452a1
Mlodzieniak <116485218+Mlodzieniak@users.noreply.github.com>
2023-02-04 08:57:06 +0100

    configured ipdata api

---
+++ src/API/ipdata.js
@@ -0,0 +1,29 @@
+function getIpData() {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+
+    request.open(
+      "GET",
+      "https://api.ipdata.co/?api-key=ba693b5e88cb1b09d451c5cfa70f9fbe53a2b1cb6feb4d4f30855b6d"
+    );
+
+    request.setRequestHeader("Accept", "application/json");
+
+    request.onreadystatechange = function () {
+      if (this.readyState === 4) {
+        if (this.status === 200) {
+          resolve(JSON.parse(this.responseText));
+        } else {
+          reject(
+            new Error(`Request failed with this status code: ${this.status}`)
+          );
+        }
+        // console.log(this.responseText);
+        // return [this.responseText.latitude, this.responseText.longitude];
+      }
+    };
+
+    request.send();
+  });
+}
+export default getIpData;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./main.css";
+import getIpData from "./API/ipdata";
 
 const submitBTN = document.querySelector(".submit-loc-btn");
 const locInput = document.querySelector(".loc-input");
@@ -6,16 +7,27 @@ const locInputError = document.querySelector(".loc-input-error");
 
 async function getWeather(location) {
   // const promise = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=079cafdb1061e25da34e6c0d52fe356d`, { mode: 'cors' });
-  const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=079cafdb1061e25da34e6c0d52fe356d`, { mode: "cors" });
+  const promise = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=079cafdb1061e25da34e6c0d52fe356d`,
+    { mode: "cors" }
+  );
   const data = await promise.json();
   console.log(data);
 }
 
-window.onload = () => {
-  if (!locInput.value.length) {
-    locInputError.className = "loc-input-error hide";
-  }
-};
+// window.onload = async () => {
+//   if (!locInput.value.length) {
+//     locInputError.className = "loc-input-error hide";
+//   }
+//   try {
+//     const ipdata = await getIpData();
+//     console.log(ipdata);
+//     console.log(`logitude ${ipdata.latitude}`);
+//     console.log(`altitude ${ipdata.longitude}`);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 locInput.oninput = () => {
   if (locInput.value.length > 2) {
     locInputError.className = "loc-input-error hide";
@@ -28,5 +40,6 @@ submitBTN.onclick = () => {
     locInputError.textContent = "You need to provide location.";
     locInputError.className = "loc-input-error";
   }
+  //   getLocation();
 };
 // getWeather('inowroclaw');
